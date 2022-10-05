c497e86a34a542d5429e3e140973f1d5c94bdedc
Jason Christopher <jchristopher2448@gmail.com>
2022-10-04 22:12:50 -0500

    added JS and fixed HTML and CSS

--- js/app.js
+++ js/app.js
@@ -1,1 +1,78 @@
 'use strict';
+let username;
+
+while(username === undefined || username === null){
+  username = prompt('What is your name');
+  if(username === undefined || username === null){
+    alert('I am not asking for much! Please try again!');
+  } else {
+    alert('Nice to meet you, ' + username + '!');
+  }
+}
+
+let locationAnswer;
+while(locationAnswer !== 'yes' && locationAnswer !== 'y' && locationAnswer !== 'no' && locationAnswer !== 'n'){
+  locationAnswer = prompt('Did I grow up in Missouri before moving to Florida in high school?').toLowerCase();
+  console.log('Location Answer: ' + locationAnswer);
+  if(locationAnswer === 'yes' || locationAnswer === 'y'){
+    alert('That is correct, ' + username + '!');
+  } else if(locationAnswer === 'no' || locationAnswer === 'n'){
+    alert('I actually was born in Missouri and lived there for 15 years before moving to Florida.');
+  } else {
+    alert('Please reply with either "yes","y","no", or "n".');
+  }
+}
+
+let collegeAnswer;
+while(collegeAnswer !== 'yes' && collegeAnswer !== 'y' && collegeAnswer !== 'no' && collegeAnswer !== 'n'){
+  collegeAnswer = prompt('Did I study Aerospace Engineering in college...even though I did not enjoy it as much as I enjoyed college football and Air Force ROTC?').toLowerCase();
+  console.log('College Answer: ' + collegeAnswer);
+  if(collegeAnswer === 'yes' || collegeAnswer === 'y'){
+    alert('That is correct, ' + username + '!');
+  } else if(collegeAnswer === 'no' || collegeAnswer === 'n'){
+    alert('I actually did not really enjoy Aerospace Engineering and spent most of my time with Air Force ROTC and cheering on the Gators.');
+  } else {
+    alert('Please reply with either "yes","y","no", or "n".');
+  }
+}
+
+let awacsAnswer;
+while(awacsAnswer !== 'yes' && awacsAnswer !== 'y' && awacsAnswer !== 'no' && awacsAnswer !== 'n'){
+  awacsAnswer = prompt('Did I join the Air Force and fly on the E-3 AWACS as an Air Battle Manager and deploy to the Middle East 3 times in support of operations against ISIS?').toLowerCase();
+  console.log('AWACS Answer: ' + awacsAnswer);
+  if(awacsAnswer === 'yes' || awacsAnswer === 'y'){
+    alert('That is correct, ' + username + '!');
+  } else if(awacsAnswer === 'no' || awacsAnswer === 'n'){
+    alert('I actually am an Air Battle Manager on the E-3 AWACS and deployed 3 times to the Middle East.');
+  } else {
+    alert('Please reply with either "yes","y","no", or "n".');
+  }
+}
+
+let familyAnswer;
+while(familyAnswer !== 'yes' && familyAnswer !== 'y' && familyAnswer !== 'no' && familyAnswer !== 'n'){
+  familyAnswer = prompt('Is my wife\'s name Kayla and do we have 2 sons, Jacob (age 3) and Ethan (age 1)?').toLowerCase();
+  console.log('Family Answer: ' + familyAnswer);
+  if(familyAnswer === 'yes' || familyAnswer === 'y'){
+    alert('That is correct, ' + username + '!');
+  } else if(familyAnswer === 'no' || familyAnswer === 'n'){
+    alert('My wife\'s name is Kayla and we have 2 sons, Jacob and Ethan.');
+  } else {
+    alert('Please reply with either "yes","y","no", or "n".');
+  }
+}
+
+let skydiveAnswer;
+while(skydiveAnswer !== 'yes' && skydiveAnswer !== 'y' && skydiveAnswer !== 'no' && skydiveAnswer !== 'n'){
+  skydiveAnswer = prompt('Did I propose to my wife while skydiving because that\'s how my parents met?').toLowerCase();
+  console.log('Skydiving Answer: ' + skydiveAnswer);
+  if(skydiveAnswer === 'yes' || skydiveAnswer === 'y'){
+    alert('That is correct, ' + username + '!');
+  } else if(skydiveAnswer === 'no' || skydiveAnswer === 'n'){
+    alert('My parents met while skydiving, so I decided to propose to my wife while skydiving.');
+  } else {
+    alert('Please reply with either "yes","y","no", or "n".');
+  }
+}
+
+alert('I hope you learned a little more about me, ' + username + '!');
